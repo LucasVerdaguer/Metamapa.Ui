f3b89b4e4d314387c36c9453f7f45ec2eb2f2ad0
LucasVerdaguer <lverdaguer@frba.utn.edu.ar>
2025-10-12 21:27:15 -0300

    agrego login y registro, tiene problemas

---
+++ src/main/java/ar/utn/ba/ddsi/gestionDeAlumnos/controllers/RegistroController.java
@@ -0,0 +1,19 @@
+package ar.utn.ba.ddsi.gestionDeAlumnos.controllers;
+
+import ar.utn.ba.ddsi.gestionDeAlumnos.dto.UsuarioDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistroController {
+
+    @GetMapping("/registro")
+    public String mostrarFormularioDeRegistro(Model model) {
+         model.addAttribute("usuarioDTO", new UsuarioDTO());
+        return "registro";
+    }
+
+    // Aquí también iría el método @PostMapping("/registro") para procesar los datos
+    // cuando el usuario envíe el formulario.
+}
